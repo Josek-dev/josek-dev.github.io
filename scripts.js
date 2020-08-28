37f7990c157eed6311412b474bebc36d7529a39a
Josek-dev <xjosekx@gmail.com>
2020-08-28 03:17:33 +0200

    toggle landing page button

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-// nothing to show yet
+jQuery.fn.extend({
+    toggleText: function (a, b) {
+        let toggle = false, that = this;
+        this.on('click', function () {
+            that.text((toggle = !toggle) ? b : a);
+        });
+        return this;
+    }
+});
+$(document).ready(function(){
+    $("#enter").on("click", function(){
+        $("#enterButton").toggleText("Enter", "Close");
+        $("#error").toggle("slow");
+
+    });
+});
